1e1de95d9f9fce0a0dab1f8475fee01083cf55a2
linzcrowe <linzcrowe@gmail.com>
2017-02-20 09:07:45 -0500

    add responsive navbar
    Add a Bootstrap styled responsive navbar

--- imports/ui/components/navigation.jsx
+++ imports/ui/components/navigation.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import { IndexLink, Link } from 'react-router';
+import AccountsUIWrapper from '../layouts/accountsUIWrapper.jsx';
 
 export const Navigation = () => (
-  <ul>
-    <li><IndexLink to="/" activeClassName="active">Introduction</IndexLink></li>
-    <li><Link to="/results" activeClassName="active" id="linkToResults">My NVL Results</Link></li>
-  </ul>
-)
+  <nav className="navbar navbar-default">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navigation-collapse-burger" aria-expanded="false">
+          <span className="sr-only">Toggle navigation</span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+        </button>
+        <a className="navbar-brand" href="#">NVL Guide</a>
+      </div>
+
+      <div className="collapse navbar-collapse" id="navigation-collapse-burger">
+        <ul className="nav navbar-nav">
+          <li><AccountsUIWrapper /></li>
+          <li><IndexLink to="/" activeClassName="active">Intro</IndexLink></li>
+          <li><Link to="/results" activeClassName="active" id="linkToResults">My NVL Results</Link></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+);
--- imports/ui/layouts/app.jsx
+++ imports/ui/layouts/app.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Navigation } from '../components/navigation.jsx';
-import AccountsUIWrapper from './accountsUIWrapper.jsx';
 
 export const App = ( { children } ) => (
   <div>
     <Navigation />
-    <AccountsUIWrapper />
     { children }
   </div>
 )
