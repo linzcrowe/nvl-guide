a4057ff7a4914db16cd6a72d58d41c7a0ad7cd73
linzcrowe <linzcrowe@gmail.com>
2017-02-23 18:24:29 -0500

    Add singular methods file
    Add a single file with all methods for front end use

---
+++ imports/api/userResults/methods.js
@@ -0,0 +1,5 @@
+import insertUserResult from './insertUserResult.js';
+import moveToLikeDrain from './moveToLikeDrain.js';
+import moveToLikeEnergise from './moveToLikeEnergise.js';
+import moveToNotLike from './moveToNotLike.js';
+import moveToShadow from './moveToShadow.js';
