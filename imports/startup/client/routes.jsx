ab8c2af4cd4d8277f6672efb1c3ddd86f99b03b9
linzcrowe <linzcrowe@gmail.com>
2017-02-25 11:51:58 -0500

    Convert to default exports
    Convert to default exports (vs. named) for React components

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
-import { App } from '../../ui/layouts/app.jsx';
-import { Index } from '../../ui/pages/index.jsx';
-import { Results } from '../../ui/pages/results.jsx';
-import { NotFound } from '../../ui/pages/not-found.jsx';
+import App from '../../ui/layouts/app.jsx';
+import Index from '../../ui/pages/index.jsx';
+import Results from '../../ui/pages/results.jsx';
+import NotFound from '../../ui/pages/not-found.jsx';
 
 Meteor.startup( () => {
   render(
--- imports/ui/pages/results.jsx
+++ imports/ui/pages/results.jsx
@@ -1,3 +1,25 @@
 import React from 'react';
 
-export const Results = () => <h1>Results</h1>;
+export default Results = (props) => {
+  const heading = <h1>Props: {JSON.stringify(props)}</h1>;
+  let body;
+
+  switch (props.stage) {
+    case 0:
+      body = <h2>First stage: Sort into piles</h2>;
+      break;
+    case 1:
+      body = <h2>Second stage: Find top 4 cards that are like you and give you the most energy</h2>;
+      break;
+    case 2:
+      body = <h2>Third stage: Find top 4 cards that are like you and drain your energy the most</h2>;
+      break;
+    case 3:
+      body = <h2>Fourth stage: Presentation of type, and link to further information</h2>;
+      break;
+    default:
+      body = <h2>Results body is broken</h2>
+  }
+
+  return (heading + body);
+}
