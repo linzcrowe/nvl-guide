543ec04d0b37ba5eff0bf6a1ec83ced078b5eb21
Lindsey Crowe <linzcrowe@users.noreply.github.com>
2017-02-25 11:50:52 -0500

    Add functional frontend (#3)
    * Add missing collection methods to methods.js
    * Fix incorrect import path that stops server from running

--- imports/api/userResults/methods.js
+++ imports/api/userResults/methods.js
@@ -3,3 +3,5 @@ import moveToLikeDrain from './moveToLikeDrain.js';
 import moveToLikeEnergise from './moveToLikeEnergise.js';
 import moveToNotLike from './moveToNotLike.js';
 import moveToShadow from './moveToShadow.js';
+import arrangeLikeDrain from './arrangeLikeDrain.js';
+import arrangeLikeEnergise from './arrangeLikeEnergise.js';
