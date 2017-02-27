b04140580b5794c473193f7a24a1b1c8e93d2039
linzcrowe <linzcrowe@gmail.com>
2017-02-26 19:07:58 -0500

    Add shell component to rank drain and energise cards

---
+++ imports/ui/layouts/rankLikeDrain.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default RankLikeDrain = (props) => {
+  return (<h1>Rank Like Drain Placeholder</h1>);
+}
---
+++ imports/ui/layouts/rankLikeEnergise.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default RankLikeEnergise = (props) => {
+  return (<h1>Rank Like Energise Placeholder</h1>);
+}
