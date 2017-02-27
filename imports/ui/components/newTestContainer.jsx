907e201bb1535d1a45c5a27d6035581f4d9704a5
linzcrowe <linzcrowe@gmail.com>
2017-02-26 19:07:10 -0500

    Add container for test component

---
+++ imports/ui/components/newTestContainer.jsx
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import insertUserResult from '../../api/userResults/insertUserResult.js';
+import NewTest from './newTest.jsx';
+import { Meteor } from 'meteor/meteor';
+
+export default createContainer(() => {
+  const isLoggedIn = Meteor.userId() !== undefined;
+  return {
+    isLoggedIn: isLoggedIn,
+    onCreate: () => insertUserResult(),
+  };
+}, NewTest);
