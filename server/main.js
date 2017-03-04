a11c0f460821dac73e599903aa4cd674d6aace57
linzcrowe <linzcrowe@gmail.com>
2017-03-04 08:01:46 -0500

    Log when server starts
    To see when the server has started and mark the start of each set of
    log entries

--- server/main.js
+++ server/main.js
@@ -1,6 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 import '../imports/startup/server/index.js';
+import Logger from '../imports/api/logger.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  Logger.info('Starting server');
 });
