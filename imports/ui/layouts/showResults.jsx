318087243a1fbd3d74a5a68dce9b7098f5e097bc
linzcrowe <linzcrowe@gmail.com>
2017-03-05 08:17:02 -0500

    Implement basic showResults

--- imports/ui/layouts/showResults.jsx
+++ imports/ui/layouts/showResults.jsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 export default ShowResults = (props) => {
-  return (<h1>Show Results Placeholder</h1>);
+  return (
+    <div>
+      <h1>Show Results Placeholder</h1>
+      <h2>LikeEnergise</h2>
+      <p>{props.likeEnergiseRanked}</p>
+      <h2>LikeDrain</h2>
+      <p>{props.likeDrainRanked}</p>
+      <h2>Shadow</h2>
+      <p>{props.shadow}</p>
+    </div>
+    );
+}
+
+ShowResults.PropTypes = {
+  likeEnergiseRanked: PropTypes.array.isRequired,
+  likeDrainRanked: PropTypes.array.isRequired,
+  shadow: PropTypes.array.isRequired,
 }
--- imports/ui/pages/results.jsx
+++ imports/ui/pages/results.jsx
@@ -1,48 +1,78 @@
 import React, {PropTypes} from 'react';
 import NewTestContainer from '../components/newTestContainer.jsx';
 import SortIntoPilesContainer from '../layouts/sortIntoPilesContainer.jsx';
-import RankLikeEnergise from '../layouts/rankLikeEnergise.jsx';
-import RankLikeDrain from '../layouts/rankLikeDrain.jsx';
+import RankLikeEnergiseContainer from '../layouts/rankLikeEnergiseContainer.jsx';
+import RankLikeDrainContainer from '../layouts/rankLikeDrainContainer.jsx';
 import ShowResults from '../layouts/showResults.jsx';
+import StageCompleteContainer from '../components/stageCompleteContainer.jsx';
+import Stages from '../../api/userResults/stages.js';
 
 export default Results = (props) => {
-  const heading = <h1>Results</h1>;
-
   let body;
   if (!Meteor.userId()) {
-    body = <h1>User is not logged in</h1>
+    body = <h1>Please login to continue.</h1>
   }
-  else if(props.results.length === 0)
-  {
+  else if(props.results.length === 0) {
+    // User doesn't have a result yet
     body = <NewTestContainer />
   } else {
     const result = props.results[0];
 
     switch (result.stage) {
-      case 0:
+      case Stages.SORT:
+        if (result.cardsRemaining.length === 0) {
+          body = <StageCompleteContainer />;
+        }
+        else {
+          body = 
+            <SortIntoPilesContainer 
+              cardsRemaining={result.cardsRemaining} 
+              shadowCards={result.shadow}
+            />;
+        }
+        break;
+      case Stages.RANK_LIKE_ENERGISE:
+        // if finished ranking || not enough in the first place to rank
+        if (result.likeEnergise.length === 0 ||
+            result.likeEnergiseRanked.length < 4) {
+          body = <StageCompleteContainer />;
+        } else {
+          body = 
+            <RankLikeEnergiseContainer 
+              ranked={result.likeEnergiseRanked}
+              toRank={result.likeEnergise}
+            />;
+        }
+        break;
+      case Stages.RANK_LIKE_DRAIN:
+        // if finished ranking || not enough in the first place to rank
+        if (result.likeDrain.length === 0 ||
+            result.likeDrainRanked.length < 4) {
+          body = <StageCompleteContainer />;
+        } else {
+          body = 
+            <RankLikeDrainContainer
+              ranked={result.likeDrainRanked}
+              toRank={result.likeDrain}
+            />;
+        }
+        break;
+      case Stages.COMPLETE:
         body = 
-          <SortIntoPilesContainer 
-            cardsRemaining={result.cardsRemaining} 
-            shadowCards={result.shadow}
+          <ShowResults
+            likeEnergiseRanked={result.likeEnergiseRanked}
+            likeDrainRanked={result.likeDrainedRanked}
+            shadow={result.shadow} 
           />;
         break;
-      case 1:
-        body = <RankLikeEnergise />;
-        break;
-      case 2:
-        body = <RankLikeDrain />;
-        break;
-      case 3:
-        body = <ShowResults />
-        break;
       default:
-        body = <h2>Error - fell through stage switch</h2>
+        throw new Error('Unknown stage: ' + result.stage);
     }
   }
 
   return (
     <div>
-      { heading }
+      <h1>Result</h1>
       { body }
     </div>
     );
