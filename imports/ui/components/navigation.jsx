c09a88e141bda5b26774a2babcc2eed79b0e897b
linzcrowe <linzcrowe@gmail.com>
2017-03-08 08:30:06 -0500

    Style navigation with creative theme
    For a better look and feel

--- imports/ui/components/navigation.jsx
+++ imports/ui/components/navigation.jsx
@@ -3,23 +3,22 @@ import { IndexLink, Link } from 'react-router';
 import AccountsUIWrapper from '../layouts/accountsUIWrapper.jsx';
 
 export const Navigation = () => (
-  <nav className="navbar navbar-default">
+  <nav className="navbar navbar-default navbar-fixed-top affix-top">
     <div className="container-fluid">
-      <div className="navbar-header">
+      <div className="navbar-header navigation-bar">
         <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navigation-collapse-burger" aria-expanded="false">
           <span className="sr-only">Toggle navigation</span>
-          <span className="icon-bar"></span>
-          <span className="icon-bar"></span>
-          <span className="icon-bar"></span>
+          Menu&nbsp;
+          <i className="fa fa-bars" aria-hidden="true"></i>
         </button>
-        <a className="navbar-brand" href="#">NVL Guide</a>
+        <a className="navbar-brand" href="#">NVL Indicator</a>
       </div>
 
       <div className="collapse navbar-collapse" id="navigation-collapse-burger">
-        <ul className="nav navbar-nav">
+        <ul className="nav navbar-nav navbar-right">
           <li><AccountsUIWrapper /></li>
           <li><IndexLink to="/" activeClassName="active">Intro</IndexLink></li>
-          <li><Link to="/results" activeClassName="active" id="linkToResults">My NVL Results</Link></li>
+          <li><Link to="/results" activeClassName="active" id="linkToResults">My Result</Link></li>
         </ul>
       </div>
     </div>
