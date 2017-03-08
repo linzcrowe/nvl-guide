3957e44dbcc900b73f5a81d54024009170a0f248
linzcrowe <linzcrowe@gmail.com>
2017-03-08 08:07:40 -0500

    import bootstrap and creative theme
    import bootstrap to set the base style
    import creative theme to customise it

--- client/main.js
+++ client/main.js
@@ -1,2 +1,6 @@
+//import '../node_modules/bootstrap-sass/assets/javascripts/boostrap.min.js';
+import 'bootstrap-sass';
+import '../imports/ui/stylesheets/creative_theme/creative.css';
 import './main.html';
 import '/imports/startup/client/routes.jsx';
+import '/imports/startup/client/accountsTemplateConfig.js';
