7b4ce8c1e4f6c6a2a6e4da9ad62a654986c51a77
linzcrowe <linzcrowe@gmail.com>
2017-03-08 08:31:03 -0500

    Create index page header
    The first part of the index page

--- imports/ui/pages/index.jsx
+++ imports/ui/pages/index.jsx
@@ -1,3 +1,26 @@
 import React from 'react';
+import LoginWrapper from '../layouts/loginWrapper.jsx';
 
-export default Index = () => <h1>Index</h1>;
+export default Index = () => (
+  <header>
+    <div className="header-content">
+      <div className="header-content-inner">
+        <h1 id="homeHeading">
+          Get more out of your day
+        </h1>
+        <hr />
+        <p>
+          Work out what energises and drains you.<br/>
+          Focus on the good, minimise the bad.<br/>
+          End your days feeling happy and relaxed.
+        </p>
+        <a href="/signup" className="btn btn-primary btn-xl page-scroll">
+          Start the test
+        </a>
+      </div>
+    </div>
+    <section>
+      <LoginWrapper />
+    </section>
+  </header>
+);
